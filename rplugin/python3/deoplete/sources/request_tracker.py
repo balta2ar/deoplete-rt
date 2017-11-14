5b148ee719960a4809b784ba96b1a5f448c459a8
Yuri Bochkarev <baltazar.bz@gmail.com>
2017-11-14 23:22:41 +0300

    enable is_volatile flag (this prevent deoplete from caching results)

--- rplugin/python3/deoplete/sources/request_tracker.py
+++ rplugin/python3/deoplete/sources/request_tracker.py
@@ -23,9 +23,17 @@ class Source(Base):
         #self.kind = 'keyword'
         self.mark = '[RT]'
         #self.min_pattern_length = 2
+
+        # Use these options if you want to filter candidates yourself
+        self.is_volatile = True
         self.matchers = []
         self.sorters = []
+
+        # Use these options if you want to implement custom matcher
+        #self.matchers = ['matcher_fuzzy', 'matcher_full_fuzzy']
+        #self.sorters = ['sorter_rank']
         #self.converters = []
+
         self.max_menu_width = 120
         self.max_abbr_width = 120
         self.input_pattern = self._source.input_pattern
@@ -34,7 +42,12 @@ class Source(Base):
         return self._source.get_complete_position(context)
 
     def gather_candidates(self, context):
+        ready, result = self._source.async_gather_candidates(context)
+        context['is_async'] = not ready
+        return result
+
+    def old_gather_candidates(self, context):
         return self._source.gather_candidates(context)
 
-    def on_post_filter(self, context):
-        return self._source.on_post_filter(context)
+    # def on_post_filter(self, context):
+    #     return self._source.on_post_filter(context)
